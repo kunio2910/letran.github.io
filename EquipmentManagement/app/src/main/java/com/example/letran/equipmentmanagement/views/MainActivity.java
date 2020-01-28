931d0f3de8c8db4cca6508857fe5b585465dae4d
letran <letrantrong93@gmail.com>
2020-01-28 16:37:10 +0700

    Revert "Update login and mainactivity"
    This reverts commit 6a7f237ee638aed58f9de2eb9393419cd0b4f5ec.

--- EquipmentManagement/app/src/main/java/com/example/letran/equipmentmanagement/views/MainActivity.java
+++ EquipmentManagement/app/src/main/java/com/example/letran/equipmentmanagement/views/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        Log.e("test1","test");
+        Log.e("test","test");
         viewPager = (ViewPager)findViewById(R.id.viewpager);
         actionBar = getSupportActionBar();
         mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
